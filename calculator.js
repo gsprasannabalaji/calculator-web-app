a884582b6e4b120a5ebd3466afc313b2b79245e7
gsprasannabalaji <govindarajalusaras.p@northeastern.edu>
2024-03-13 19:09:49 -0400

    calculator and login page

---
+++ calculator.js
@@ -0,0 +1,81 @@
+$(document).ready(function () {
+
+    $('#welcome-message').text(`Welcome, ${sessionStorage.getItem('username')}`);
+    $(".error-message").hide();
+
+    const validate = (num, errorId) => {
+        let regex = /^[0-9.eE+-]+$/g;
+        if(num === '') {
+            $(errorId).text(`${errorId == "#number1Error" ? 'Number 1' : 'Number 2'} is required.`);
+            $(errorId).show();
+        } else if(!isFinite(parseFloat(num))) {
+            $(errorId).text(`${errorId == "#number1Error" ? 'Number 1' : 'Number 2'} must be a finite number.`);
+            $(errorId).show();
+        } else if(!regex.test(num)) {
+            $(errorId).text(`${errorId == "#number1Error" ? 'Number 1' : 'Number 2'} field will only accept numbers. Special Characters is not allowed`);
+            $(errorId).show();
+        }
+        else {
+            $(errorId).hide();
+        }
+    }
+
+    const isValidNumbers = (num1, num2) => {
+        let isValid = false;
+        
+        validate(num1, "#number1Error");
+        validate(num2, "#number2Error");
+
+        if(num1 !== '' && num2 !== '' && isFinite(parseFloat(num1)) && isFinite(parseFloat(num2))) {
+            isValid = true;
+            $(".error-message").hide();
+        }
+ 
+        return isValid;
+    }
+
+    $("#number1").on("input", () => {
+        let num1 = $('#number1').val();
+        validate(num1, "#number1Error");
+    });
+
+    $("#number2").on("input", () => {
+        let num2 = $('#number2').val();
+        validate(num2, "#number2Error");
+    });
+
+    $('#add, #subtract, #multiply, #divide').on('click', (event) => {
+        let num1 = $('#number1').val();
+        let num2 = $('#number2').val();
+        if(isValidNumbers(num1, num2)) {
+            let operator = $(event.target).data('operation');
+            let result = 0;
+            switch (operator) {
+                case '+':
+                    result = parseFloat(num1) + parseFloat(num2);
+                    break;
+                case '-':
+                    result = parseFloat(num1) - parseFloat(num2);
+                    break;
+                case '*':
+                    result = parseFloat(num1) * parseFloat(num2);
+                    break;
+                case '/':
+                    if((parseFloat(num1) / parseFloat(num2)) == Infinity){
+                        result = "Not a number";
+                        $('#number2Error').text("For division by zero is not allowed.");
+                        $('#number2Error').show();
+                    } else {
+                        result = parseFloat(num1) / parseFloat(num2);
+                        $('#number2Error').hide();
+                    }
+                    break;
+                default:
+                    break;
+            }
+            // result = Number.isInteger(result)? result : result.toFixed(2);
+            $('#result').val(result);
+        }
+    });
+
+});
---
+++ login.js
@@ -0,0 +1,146 @@
+$(document).ready(function () {
+  $("#loginButton").prop("disabled", true);
+
+  let isEmailValid = false;
+  let isUsernameValid = false;
+  let isPasswordValid = false;
+  let isConfirmPasswordValid = false;
+
+  $("#email").val('');
+  $("#username").val('');
+
+  $(".form__error").prop("display", "none");
+
+  const northeasternEmailRegex = /^[a-zA-Z0-9._%+-]+@northeastern\.edu$/;
+
+  // Function to check if the email is valid northeastern email
+  const isValidNortheasternEmail = (email) => {
+    return northeasternEmailRegex.test(email);
+  };
+
+  const isValidPassword = (password) => {
+    const hasSpecialCharacter = /[!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/.test(
+      password
+    );
+    const hasCapitalLetter = /[A-Z]/.test(password);
+    const hasNumber = /\d/.test(password);
+
+    if(hasSpecialCharacter && hasCapitalLetter && hasNumber && (!(password.length < 8) && !(password.length > 20))) {
+      return true;
+    } else {
+      let errorText = "Password must contain at least <br>";
+      if(!hasSpecialCharacter) {
+        errorText += " one special character <br>";
+      } 
+      if(!hasCapitalLetter) {
+        errorText += " one capital letter <br>";
+      }
+      if(!hasNumber) {
+        errorText += " one number <br>";
+      }
+      if(password.length < 8 || password.length > 20) {
+        errorText += " characters length must be between 8 and 20.";
+      }
+      $("#passwordError").html(
+        errorText
+      );
+      return false;
+    }
+
+  };
+
+  const confirmPasswordValidation = () => {
+    const confirmPassword = $("#confirmPassword").val();
+    const password = $("#password").val();
+    isConfirmPasswordValid = false;
+    if(confirmPassword && password === confirmPassword) {
+        isConfirmPasswordValid = true;
+        $("#confirmPasswordError").hide();
+    }
+    return !confirmPassword
+      ? "Confirm Password is required."
+      : password !== confirmPassword
+      ? "Password do not match."
+      : "";
+  };
+
+  $("#email").on("input", function () {
+    const email = $(this).val();
+    isEmailValid = false;
+    if (!email) {
+      $("#emailError").text("Email is required.");
+      $("#emailError").show();
+    } else {
+      if (!isValidNortheasternEmail(email)) {
+        $("#emailError").text(
+          "Please enter a valid northeastern email address."
+        );
+        $("#emailError").show();
+      } else {
+        $("#emailError").text("");
+        $("#emailError").hide();
+        isEmailValid = true;
+      }
+    }
+    updateLoginButtonState();
+  });
+
+  $("#username").on("input", function () {
+    const username = $(this).val();
+    isUsernameValid = false;
+    if (!username) {
+      $("#usernameError").text("Username is required.");
+      $("#usernameError").show();
+    } else {
+      $("#usernameError").text("");
+      $("#usernameError").hide();
+      isUsernameValid = true;
+    }
+    updateLoginButtonState();
+  });
+
+  $("#password").on("input", function () {
+    const password = $(this).val();
+    isPasswordValid = false;
+    if (!password) {
+      $("#passwordError").text("Password is required.");
+    } else {
+      if (!isValidPassword(password)) {
+        $("#passwordError").show();;
+      } else {
+        $("#passwordError").text("");
+        $("#passwordError").hide();
+        isPasswordValid = true;
+      }
+    }
+    if($('#confirmPassword').val()!== "") {
+        const confirmPassError = confirmPasswordValidation();
+        $("#confirmPasswordError").text(confirmPassError);
+    }
+    updateLoginButtonState();
+  });
+
+  $("#confirmPassword").on("input", function () {
+    const confirmPassError = confirmPasswordValidation();
+    $("#confirmPasswordError").text(confirmPassError);
+    updateLoginButtonState();
+  });
+
+  $("#loginButton").on("click", function (event) {
+    event.preventDefault();
+    sessionStorage.setItem("username", $("#username").val());
+    window.location.href = "calculator.html";
+  });
+
+  const updateLoginButtonState = () => {
+    $("#loginButton").prop(
+      "disabled",
+      !(
+        isEmailValid &&
+        isUsernameValid &&
+        isPasswordValid &&
+        isConfirmPasswordValid
+      )
+    );
+  };
+});
